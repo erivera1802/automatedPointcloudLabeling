78136e1c4a8d86934908f7ee35df76621d75ba86
hurile <ge87nec@mytum.de>
2025-02-10 14:12:53 +0000

    rosbag process and creating pkl file for each sequence

--- tools/cfgs/target_custom/label_generation/round1_automated_pipeline/scripts/automated_pipeline_general.sh
+++ tools/cfgs/target_custom/label_generation/round1_automated_pipeline/scripts/automated_pipeline_general.sh
@@ -7,6 +7,10 @@ DIR="/MS3D/data/edgar/TO_PSEUDO_LABEL"
 # DIR="/MS3D/data/manual_annotation_edgar"
 SAVE_DIR=""
 
+# Process rosbag folders and prepare LiDAR data for pseudo-labeling.
+python3 /MS3D/utils/autopipeline_data.py --save_dir "/MS3D/data/edgar/TO_PSEUDO_LABEL"
+
+
 # Parse command-line arguments
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -29,17 +33,21 @@ for rosbag_folder in ${DIR}/*; do
     if [[ -d "$rosbag_folder" ]]; then
         echo "Processing folder: $rosbag_folder"
 
-        # Step 1: Generate LiDAR odometry
+        # Step 1: Generate .pkl file
+        echo "Generating .pkl file for $rosbag_folder"
+        python3 -m pcdet.datasets.custom.custom_dataset create_infos "$rosbag_folder"
+
+        # Step 2: Generate LiDAR odometry
         echo "Generating LiDAR odometry in $rosbag_folder"
         cd "$rosbag_folder"
         bash generate_lidar_odom.sh
         cd - > /dev/null
 
-        # Step 2: Update YAML configuration
+        # Step 3: Update YAML configuration
         echo "Updating YAML configuration for $rosbag_folder"
         python3 cfgs/target_custom/label_generation/round1_automated_pipeline/scripts/update_custom_dataset_da_yaml.py --mcap_path "$rosbag_folder"
 
-        # Step 3: Run the automated pipeline
+        # Step 4: Run the automated pipeline
         echo "Running automated pipeline for $rosbag_folder"
         bash cfgs/target_custom/label_generation/round1_automated_pipeline/scripts/automated_pipeline.sh --save_dir_label "${SAVE_DIR}"
 
